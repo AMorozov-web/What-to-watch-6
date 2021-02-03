39766e40ceee4771da37f18046871127f2bfcb7c
AMorozov-web <AMorozov-web@users.noreply.github.com>
2021-02-03 15:08:56 +0300

    feat: added small card component, included it in main page component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MainPage from '../main-page/main-page';
 
-const App = () => {
+const App = (props) => {
+  const {titles} = props;
 
   return (
-    <MainPage />
+    <MainPage
+      titles={titles}
+    />
   );
+};
+
+App.propTypes = {
+  titles: PropTypes.arrayOf(PropTypes.string.isRequired),
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const TITLES = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `We need to talk about Kevin`,
+  `What We Do in the Shadows`,
+  `Revenant`,
+  `Johnny English`,
+  `Shutter Island`,
+  `Pulp Fiction`,
+  `No Country for Old Men`,
+  `Snatch`,
+  `Moonrise Kingdom`,
+  `Seven Years in Tibet`,
+  `Midnight Special`,
+  `War of the Worlds`,
+  `Dardjeeling Limited`,
+  `Orlando`,
+  `Mindhunter`,
+  `Midnight Special`
+];
+
 ReactDOM.render(
-  <App />,
-  document.querySelector(`#root`)
+    <App
+      titles={TITLES}
+    />,
+    document.querySelector(`#root`)
 );
