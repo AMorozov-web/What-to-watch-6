122ffbe0f21119bc3594a6f0614fabebea845aa5
AlexM <a.morozov.work@mail.ru>
2021-02-07 17:49:15 +0300

    feat: Added routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import MainPage from '../main-page/main-page';
+import SignInPage from '../sign-in-page/sign-in-page';
+import MyListPage from '../my-list-page/my-list-page';
+import FilmPage from '../film-page/film-page';
+import AddReviewPage from '../add-review-page/add-review.page';
+import PlayerPage from '../player-page/player-page';
+import NotFoundPage from '../not-found-page/not-found-page';
 
 const App = ({titles}) => {
 
   return (
-    <MainPage
-      titles={titles}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage titles={titles} />
+        </Route>
+        <Route exact path="/login">
+          <SignInPage />
+        </Route>
+        <Route exact path="/mylist">
+          <MyListPage />
+        </Route>
+        <Route exact path="/films/:id?">
+          <FilmPage />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReviewPage />
+        </Route>
+        <Route exact path="/player/:id?">
+          <PlayerPage />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
