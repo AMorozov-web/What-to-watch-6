9e15977685056262333514eae3480c23a86bc333
AlexM <a.morozov.work@mail.ru>
2021-02-19 20:23:51 +0300

    feat: added rating text function

--- src/consts.js
+++ src/consts.js
@@ -20,6 +20,22 @@ const filmTypeReview = PropTypes.shape({
   videoLink: PropTypes.string.isRequired,
 });
 
+const getRatingText = (rating) => {
+  switch (rating) {
+    case rating >= 0 && rating < 3:
+      return `Bad`;
+    case rating >= 3 && rating < 5:
+      return `Normal`;
+    case rating >= 5 && rating < 8:
+      return `Good`;
+    case rating >= 8 && rating < 10:
+      return `Very good`;
+    default:
+      return `Awesome`;
+  }
+};
+
 export {
   filmTypeReview,
+  getRatingText,
 };
