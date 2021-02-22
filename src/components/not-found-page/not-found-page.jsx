62d6fee2238f38d5b2b3afc77bde6fe13a90f104
AlexM <a.morozov.work@mail.ru>
2021-02-22 11:29:03 +0300

    feat: added styles to 404 page

--- src/components/not-found-page/not-found-page.jsx
+++ src/components/not-found-page/not-found-page.jsx
@@ -3,15 +3,34 @@ import {Link} from 'react-router-dom';
 import Logo from '../logo/logo';
 
 const NotFoundPage = () => {
+  const style = {
+    h1: {
+      textAlign: `center`,
+    },
+    h2: {
+      textAlign: `center`,
+    },
+    link: {
+      display: `flex`,
+      justifyContent: `center`,
+      alignItems: `center`,
+      height: `54px`,
+      color: `#d9cd8d`,
+      textDecoration: `none`,
+      border: `1px solid rgba(223,207,119,.36)`,
+      borderRadius: `8px`,
+    },
+  };
+
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
         <Logo />
       </header>
       <div className="sign-in user-page__content">
-        <h1>404.</h1>
-        <h2>Page Not Found</h2>
-        <Link to="/" className="logo__link">
+        <h1 style={style.h1}>404.</h1>
+        <h2 style={style.h2}>Page Not Found</h2>
+        <Link to="/" style={style.link}>
           <span>Go to Main page</span>
         </Link>
       </div>
