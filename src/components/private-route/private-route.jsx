5322c0c5198977e8830c59f17fb54d629d758ff6
Alex Morozov <a.morozov.work@mail.ru>
2021-03-21 20:19:54 +0300

    feat: create private-route component

---
+++ src/components/private-route/private-route.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Redirect, Route} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {selectUser} from '../../store/reducers/user/selectors';
+import {AppRoute, AuthorizationStatus} from '../../consts';
+
+const PrivateRoute = ({render, path, exact}) => {
+  const {authorizationStatus} = useSelector(selectUser);
+
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={(routeProps) =>
+        authorizationStatus === AuthorizationStatus.AUTH ? render(routeProps) : <Redirect to={AppRoute.LOGIN}/>
+      }
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  path: PropTypes.string.isRequired,
+  exact: PropTypes.bool.isRequired,
+  render: PropTypes.func.isRequired,
+};
+
+export {PrivateRoute};
