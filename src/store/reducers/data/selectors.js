870e934755fd5a27a36b8bc08449c9b5c2780542
AMorozov-web <AMorozov-web@users.noreply.github.com>
2021-03-24 11:48:17 +0300

    refactor: start refactoring data selectors

--- src/store/reducers/data/selectors.js
+++ src/store/reducers/data/selectors.js
@@ -2,10 +2,13 @@ import {createSelector} from 'reselect';
 import {Genre} from '../../../consts';
 
 const selectData = (state) => state.DATA;
+const selectAllFilms = (state) => state.DATA.films;
+const selectGenre = (state) => state.DATA.selectedGenre;
 
 const selectFilmsByGenre = createSelector(
-    selectData,
-    ({films, selectedGenre}) =>
+    selectAllFilms,
+    selectGenre,
+    (films, selectedGenre) =>
       selectedGenre === Genre.ALL ? films : films.filter((film) => film.genre.toLowerCase() === selectedGenre)
 );
 
