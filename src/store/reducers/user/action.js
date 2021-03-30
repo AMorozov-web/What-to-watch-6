7fcf3b2e95d9600ff29494af1f0eb31d003728ba
AMorozov-web <AMorozov-web@users.noreply.github.com>
2021-03-30 18:40:41 +0300

    init: start dev error-event functionality

--- src/store/reducers/user/action.js
+++ src/store/reducers/user/action.js
@@ -3,6 +3,8 @@ import {createAction} from '@reduxjs/toolkit';
 const ActionType = {
   REQUIRED_AUTHORIZATION: `user/requiredAuthorization`,
   SET_AUTH_INFO: `user/setAuthenticationInfo`,
+  SET_FORM_DISABLED: `user/setFormDisabled`,
+  SET_ERROR_MESSAGE: `user/setErrorMessage`,
 };
 
 const requireAuthorization = createAction(ActionType.REQUIRED_AUTHORIZATION, (status) => {
@@ -11,13 +13,27 @@ const requireAuthorization = createAction(ActionType.REQUIRED_AUTHORIZATION, (st
   };
 });
 
-const setAuthInfo = createAction(ActionType.SET_AUTH_INFO, (status) => {
+const setAuthInfo = createAction(ActionType.SET_AUTH_INFO, (info) => {
+  return {
+    payload: info,
+  };
+});
+
+const setFormDisabled = createAction(ActionType.SET_FORM_DISABLED, (status) => {
   return {
     payload: status,
+  };
+});
+
+const setErrorMessage = createAction(ActionType.SET_ERROR_MESSAGE, (errorMessage) => {
+  return {
+    payload: errorMessage,
   };
 });
 
 export {
   requireAuthorization,
   setAuthInfo,
+  setFormDisabled,
+  setErrorMessage,
 };
