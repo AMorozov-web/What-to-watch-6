0050827cc92e8e093a16205683f0c3347cba97a6
Alex Morozov <a.morozov.work@mail.ru>
2021-04-04 14:45:02 +0300

    feat: add player-time-progress tests

--- src/components/player-time-progress/player-time-progress.jsx
+++ src/components/player-time-progress/player-time-progress.jsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 const PlayerTimeProgress = ({progressPercent}) => {
 
   return (
-    <div className="player__time">
+    <div className="player__time" data-testid="progress">
       <progress className="player__progress" value={progressPercent ? progressPercent : 0} max={100} />
-      <div className="player__toggler" style={{left: `${progressPercent}%`}}>Toggler</div>
+      <div className="player__toggler" style={{left: `${progressPercent}%`}} data-testid="progress-toggler">Toggler</div>
     </div>
   );
 };
