a808548ab4eecd754fb7336575769360d00038a5
AMorozov-web <AMorozov-web@users.noreply.github.com>
2021-04-05 12:38:05 +0300

    fix: fix form disabling on submit, fix error message timeout

--- src/components/comments-form/comments-form.jsx
+++ src/components/comments-form/comments-form.jsx
@@ -1,31 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {useDispatch, useSelector} from 'react-redux';
-import {selectErrorMessage} from '../../store/reducers/user/selectors';
+import {setErrorMessage} from '../../store/reducers/user/action';
+import {selectErrorMessage, selectFormDisabled} from '../../store/reducers/user/selectors';
 import {sendReview} from '../../store/api-actions';
 import {RATING_STARS_COUNT, ReviewTextValidation} from '../../consts';
 
-const getStar = (value) => {
-
-  return (
-    <React.Fragment key={`star-${value}`}>
-      <input className="rating__input" id={`star-${value}`} type="radio" name="rating" defaultValue={value} />
-      <label className="rating__label" htmlFor={`star-${value}`}>{`Rating ${value}`}</label>
-    </React.Fragment>
-  );
-};
-
-const ratingStars = Array(RATING_STARS_COUNT).fill().map((_, i) => getStar(i));
-
-const CommentForm = ({id}) => {
-  const errorMessage = useSelector(selectErrorMessage);
-  const [review, setReview] = useState({
-    rating: 0,
-    comment: ``,
-  });
-  const [isCommentValid, setCommentValid] = useState(false);
+const getErrorMessage = (errorMessage) => {
   const dispatch = useDispatch();
-
   const styles = {
     display: `block`,
     width: `auto`,
@@ -37,6 +19,25 @@ const CommentForm = ({id}) => {
     border: `2px solid rgba(252, 7, 7, 0.5)`,
     borderRadius: `8px`,
   };
+
+  setTimeout(() => {
+    dispatch(setErrorMessage(null));
+  }, 3000);
+
+  return (
+    <p style={styles}>{`${errorMessage}. Please try again later`}</p>
+  );
+};
+
+const CommentForm = ({id}) => {
+  const errorMessage = useSelector(selectErrorMessage);
+  const isFormDisabled = useSelector(selectFormDisabled);
+  const [review, setReview] = useState({
+    rating: 0,
+    comment: ``,
+  });
+  const [isCommentValid, setCommentValid] = useState(false);
+  const dispatch = useDispatch();
 
   const handleRatingChange = (evt) => {
     const {defaultValue} = evt.target;
@@ -61,6 +62,18 @@ const CommentForm = ({id}) => {
     setCommentValid(isRatingValid && isTextValid);
   }, [review]);
 
+  const getStar = (value) => {
+
+    return (
+      <React.Fragment key={`star-${value}`}>
+        <input className="rating__input" id={`star-${value}`} type="radio" name="rating" defaultValue={value} disabled={isFormDisabled} />
+        <label className="rating__label" htmlFor={`star-${value}`}>{`Rating ${value}`}</label>
+      </React.Fragment>
+    );
+  };
+
+  const ratingStars = Array(RATING_STARS_COUNT).fill().map((_, i) => getStar(i));
+
   return (
     <form onSubmit={handleSubmit} className="add-review__form" data-testid={`add-review-form`}>
       <div className="rating">
@@ -77,12 +90,13 @@ const CommentForm = ({id}) => {
           minLength={ReviewTextValidation.MIN_TEXT_LENGTH}
           maxLength={ReviewTextValidation.MAX_TEXT_LENGTH}
           onChange={handleTextChange}
-          data-testid={`add-review-textarea`}/>
+          data-testid={`add-review-textarea`}
+          disabled={isFormDisabled}/>
         <div className="add-review__submit">
-          <button className="add-review__btn" type="submit" disabled={!isCommentValid}>Post</button>
+          <button className="add-review__btn" type="submit" disabled={!isCommentValid || isFormDisabled}>Post</button>
         </div>
       </div>
-      {errorMessage && <p style={styles}>{`${errorMessage}. Please try again later`}</p>}
+      {errorMessage && getErrorMessage(errorMessage)}
     </form>
   );
 };
